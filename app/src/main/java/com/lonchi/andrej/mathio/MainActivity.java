9543fff7958fd478a9eb7ec3e4688e2db8f54413
Lonchi78 <andrejloncik@gmail.com>
2018-01-15 18:26:36 +0100

    Added one variable in "MainActivity.java"

--- app/src/main/java/com/lonchi/andrej/mathio/MainActivity.java
+++ app/src/main/java/com/lonchi/andrej/mathio/MainActivity.java
@@ -2,13 +2,73 @@ package com.lonchi.andrej.mathio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+    Button mButton1;
+    Button mButton2;
+
+    //  Celkovy koren realtime databazy
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    //  Odkaz ku premennej "condition", ktora je pod mRootRef
+    DatabaseReference mConditionRef = mRootRef.child("condition");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Get UI elements
+        mTextView = findViewById(R.id.tv_try);
+        mButton1 = findViewById(R.id.btn_try1);
+        mButton2 = findViewById(R.id.btn_try2);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //  Zachytava kazdu zmenu hodnoty u "condition"
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                mTextView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //  Po klilknuti nastavi hodnotu na "Rainy"
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mConditionRef.setValue("Rainy");
+            }
+        });
+
+        //  Po kliknuti nastavi hodnotu na "Sunny"
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mConditionRef.setValue("Sunny");
+            }
+        });
+
     }
 
     private void testUpload() {
